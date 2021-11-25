c96848699e1e3fffa0831050710f44dcb01aa102
Richard Munn <richard.munn@arup.com>
2021-11-25 07:46:27 +0000

    Added empty source file and build task

---
+++ collapse_nodes.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+void test_remove_coincident_points(){
+
+    cout << "No code yet" << endl;
+
+}
+
+int main(){
+    test_remove_coincident_points();
+}
